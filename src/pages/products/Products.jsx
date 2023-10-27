f65fe47ef0dcc66d095d5031b250f574576cb2df
Florencia Allami <allami.florencia@gmail.com>
2023-10-27 10:47:52 -0300

    basic grid structure and card view

---
+++ src/pages/products/Products.jsx
@@ -0,0 +1,124 @@
+import React from 'react'
+import {Card, CardBody, CardFooter, Image} from "@nextui-org/react";
+
+// the key should be the id
+// on press should navigate
+
+function Products() {
+    const list = [
+        {
+          title: "Orange",
+          img: "https://nextui.org/images/fruit-1.jpeg",
+          price: "$5.50",
+        },
+        {
+          title: "Tangerine",
+          img: "https://nextui.org/images/fruit-2.jpeg",
+          price: "$3.00",
+        },
+        {
+          title: "Raspberry",
+          img: "https://nextui.org/images/fruit-3.jpeg",
+          price: "$10.00",
+        },
+        {
+          title: "Lemon",
+          img: "https://nextui.org/images/fruit-4.jpeg",
+          price: "$5.30",
+        },
+        {
+          title: "Avocado",
+          img: "https://nextui.org/images/fruit-5.jpeg",
+          price: "$15.70",
+        },
+        {
+          title: "Lemon 2",
+          img: "https://nextui.org/images/fruit-6.jpeg",
+          price: "$8.00",
+        },
+        {
+          title: "Banana",
+          img: "https://nextui.org/images/fruit-7.jpeg",
+          price: "$7.50",
+        },
+        {
+          title: "Watermelon",
+          img: "https://nextui.org/images/fruit-8.jpeg",
+          price: "$12.20",
+        },
+        {
+          title: "Orange",
+          img: "https://nextui.org/images/fruit-1.jpeg",
+          price: "$5.50",
+        },
+        {
+          title: "Tangerine",
+          img: "https://nextui.org/images/fruit-2.jpeg",
+          price: "$3.00",
+        },
+        {
+          title: "Raspberry",
+          img: "https://nextui.org/images/fruit-3.jpeg",
+          price: "$10.00",
+        },
+        {
+          title: "Lemon",
+          img: "https://nextui.org/images/fruit-4.jpeg",
+          price: "$5.30",
+        },
+        {
+          title: "Avocado",
+          img: "https://nextui.org/images/fruit-5.jpeg",
+          price: "$15.70",
+        },
+        {
+          title: "Lemon 2",
+          img: "https://nextui.org/images/fruit-6.jpeg",
+          price: "$8.00",
+        },
+        {
+          title: "Banana",
+          img: "https://nextui.org/images/fruit-7.jpeg",
+          price: "$7.50",
+        },
+        {
+          title: "Watermelon",
+          img: "https://nextui.org/images/fruit-8.jpeg",
+          price: "$12.20",
+        },
+        {
+          title: "Orange",
+          img: "https://nextui.org/images/fruit-1.jpeg",
+          price: "$5.50",
+        },
+        {
+          title: "Tangerine",
+          img: "https://nextui.org/images/fruit-2.jpeg",
+          price: "$3.00",
+        },
+      ];
+
+  return (
+    <div className="gap-2 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 m-10 lg:mx-20">
+      {list.map((item, index) => (
+        <Card shadow="sm" key={index} className='hover:scale-105 transition-scale duration-75 rounded-sm' isPressable isHoverable={true} onPress={() => console.log("item pressed")}>
+          <CardBody className="overflow-visible p-0">
+            <Image
+              shadow="sm"
+              width="100%"
+              alt={item.title}
+              className="w-full object-cover h-[250px]"
+              src={item.img}
+            />
+          </CardBody>
+          <CardFooter className="text-small flex justify-between">
+            <b>{item.title}</b>
+            <p className="text-default-500">{item.price}</p>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  )
+}
+
+export default Products
