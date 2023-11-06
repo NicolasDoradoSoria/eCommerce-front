0918d167c751a635928350dbdcb3018b6356dcc4
Florencia Allami <allami.florencia@gmail.com>
2023-11-06 16:40:32 -0300

    controlled filters in search results

--- src/app/search/[searchKey]/components/FilterOptions.jsx
+++ src/app/search/[searchKey]/components/FilterOptions.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
+import {Radio, RadioGroup, Checkbox, CheckboxGroup, Button } from "@nextui-org/react"
+import { useControlledForm } from '../hooks/useControlledForm'
+import { filtros } from '../temporaryData'
 
 function FilterOptions() {
+  const {handleChange, handleSubmit, isCheckbox} = useControlledForm()
+
   return (
-    <div className='ms-16 mt-20'>
-      <ul>
-        <li>hi</li>
-        <li>hello</li>
-        <li>something longer</li>
-      </ul>
-    </div>
+    <form className='ms-16 mt-20'>
+      <Button variant="ghost" color='primary' onPress={handleSubmit} className='my-3'>Filtrar</Button>
+      {filtros.map((seccion, i) => {
+        return (
+        <div key={i} className='my-3'>
+          {(isCheckbox(seccion.type)) ? (
+            <CheckboxGroup label={seccion.title} onValueChange={(value)=>handleChange(seccion.title, value)}>
+              {seccion.options.map((opcion, i)=> {
+              return (
+                <Checkbox key={i} value={opcion}>{opcion}</Checkbox>
+              )
+            })}
+            </CheckboxGroup>
+            )
+            : (
+            <RadioGroup label={seccion.title} onValueChange={(value) => handleChange(seccion.title, value)}>
+              {seccion.options.map((opcion, i)=> {
+              return (
+              <Radio key={i} value={opcion}>
+                {opcion}
+              </Radio>)
+            })}
+            </RadioGroup>
+            )
+          }
+        </div>)
+      })}
+    </form>
   )
 }
 
---
+++ src/app/search/[searchKey]/temporaryData.js
@@ -0,0 +1,11 @@
+export const filtros = [
+    {
+      title: "Género",
+      type: "radio",
+      options: ["Mujer", "Hombre", "Niño", "Bebé"]
+    }, {
+      title: "Color",
+      type: "checkbox",
+      options: ["Rojo", "Azul", "Verde"]
+    }
+]
